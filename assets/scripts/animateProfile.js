c5a86203d004cab657ce030378a482abbb4e0222
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-01 13:51:03 +0900

    feat(profile): animate owner name

---
+++ assets/scripts/animateProfile.js
@@ -0,0 +1,14 @@
+$('.profile__name .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({
+  easing: 'easeOutExpo',
+  duration: 750
+})
+  .add({
+    targets: '.profile__name .letter',
+    scale: [0, 1],
+    duration: 2000,
+    elasticity: 500,
+  })
