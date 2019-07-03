b6dddde84f9657dc176aa78e28e671d7a5b88dab
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-03 21:11:16 +0900

    feat(projects): redesign search ui

--- assets/scripts/searchProject.js
+++ assets/scripts/searchProject.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $('#search').on('keyup', function() {
     const value = $.trim($(this).val().toLowerCase());
+
+    $('#question').text(
+      cox.postposition.pick(value, '로') + ' 세상을 바꿉니다.'
+    )
+
     $('.projects a').each(function() {
       if ($(this).text().toLowerCase().indexOf(value) <= -1)
         $(this).css('display', 'none');
